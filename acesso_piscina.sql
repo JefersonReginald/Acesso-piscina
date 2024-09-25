a68ce9534cc9a293079bb62828934b91e3fa703b
Jeferson <99222347+JefersonReginald@users.noreply.github.com>
2024-09-25 20:32:43 -0300

    Update acesso_piscina.sql

--- acesso_piscina.sql
+++ acesso_piscina.sql
@@ -1,11 +1,8 @@
 USE controle_acesso_piscina;
--- Criar o banco de dados
 CREATE DATABASE controle_acesso_piscina;
 
--- Usar o banco de dados criado
 USE controle_acesso_piscina;
 
--- Criar a tabela de Usuários (moradores)
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
@@ -15,7 +12,6 @@ CREATE TABLE usuarios (
     telefone VARCHAR(15)
 );
 
--- Criar a tabela de Acessos (registros de acesso à piscina)
 CREATE TABLE acessos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     usuario_id INT,
@@ -24,7 +20,6 @@ CREATE TABLE acessos (
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
 );
 
--- Criar a tabela de Regras de Acesso (regras de uso da piscina)
 CREATE TABLE regras_acesso (
     id INT AUTO_INCREMENT PRIMARY KEY,
     descricao TEXT NOT NULL,
@@ -33,7 +28,6 @@ CREATE TABLE regras_acesso (
     max_pessoas INT NOT NULL
 );
 
--- Inserir algumas regras iniciais de acesso
 INSERT INTO regras_acesso (descricao, horario_inicio, horario_fim, max_pessoas)
 VALUES 
 ('A piscina pode ser utilizada de segunda a sexta-feira, das 8h às 18h', '08:00:00', '18:00:00', 20),
@@ -46,8 +40,6 @@ VALUES
 ('Jaqueline Ferreira', 'jaqueline.ferreira@example.com', 'jaqueline123', 'D404', '(41) 95555-5555'),
 ('Simone Cienslinki', 'simone.cienslinki@example.com', 'simone123', 'E505', '(41) 94444-4444');
 
-
--- Inserir registros de acesso para teste
 INSERT INTO acessos (usuario_id, status_acesso) 
 VALUES 
 (1, 'Autorizado'),
@@ -57,10 +49,8 @@ VALUES
 (5, 'Negado'),
 (6, 'Autorizado');
 
--- Consulta para verificar as regras de acesso
 SELECT * FROM regras_acesso;
 
--- Consulta para listar todos os usuários e seus acessos
 SELECT u.nome, u.apartamento, a.data_acesso, a.status_acesso
 FROM usuarios u
 JOIN acessos a ON u.id = a.usuario_id;
